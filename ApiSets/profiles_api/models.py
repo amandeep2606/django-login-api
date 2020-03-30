aa47ad71e5d22ee59ca6f12330fa3e03621d9d36
asingh0109 <amandeep.singh@equinox.com>
2020-03-30 17:31:43 +0530

    added profile viewset

--- ApiSets/profiles_api/models.py
+++ ApiSets/profiles_api/models.py
@@ -60,7 +60,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def get_full_name(self):
         """
--- ApiSets/profiles_api/views.py
+++ ApiSets/profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from profiles_api.models import UserProfile
+from profiles_api.serializers import UserProfileSerializer
+from profiles_api.permissions import UpdateOwnProfile
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
+
+
+class UserProfileViewset(viewsets.ModelViewSet):
+    """
+    viiewset for UserProfile Model
+    """
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
+
